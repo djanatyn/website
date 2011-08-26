9cd77bffc060c127c4c2fd1b3cb051cc0d503009
Jonathan <djanatyn@gmail.com>
2011-08-25 20:05:45 -0400

    some small changes

--- generate.py
+++ generate.py
@@ -9,9 +9,10 @@ root = tree.getroot()
 template = env.get_template('entry.html')
 entry_num = 100
 for entry in root:
-    fh = open("%d.html" % entry_num,'w')
+    fh = open("./output/%d.html" % entry_num,'w')
+    text = entry[2].text
     fh.write(template.render(
             title = entry[0].text,
             date = entry[1].text,
-            text = entry[2].text))
+            text = text.replace('\n','</p><p>')))
     entry_num += 1
